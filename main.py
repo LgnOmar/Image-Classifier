888765bbca90b07494eabee592b3d8ca7af59892
LAGGOUNE Omar <dogie4bogie@gmail.com>
2025-04-09 07:23:56 +0200

    Done with image classification
    Dont with image classifier using Keras and TensorFlow.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 from tensorflow.keras.utils import to_categorical
+import matplotlib.pyplot as plt
 
 
 # Load the MNIST dataset
@@ -11,8 +12,50 @@ from tensorflow.keras.utils import to_categorical
 train_images = train_images/255.0
 test_images = test_images/255.0
 
-#reshape the images to (28, 28, 1) as they are greyscale
+# Reshape the images to (28, 28, 1) as they are greyscale
 train_images = train_images.reshape((train_images.shape[0], 28, 28, 1))
 test_images = test_images.reshape((test_images.shape[0], 28, 28, 1))
 
-#convert the labels to one-hot encoded format
+# Convert the labels to one-hot encoded format
+train_labels= to_categorical(train_labels)
+test_labels= to_categorical(test_labels)
+
+# Build the CNN Model
+model = models.Sequential()
+
+# First Convolutional Layer
+model.add(layers.Conv2D(32,(3,3), activation='relu', input_shape=(28,28,1)))
+model.add(layers.MaxPooling2D((2,2)))
+
+# Second Convolutional Layer
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+
+# Third Convolutional Layer
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+
+# Flatten the 3D output to 1D and add a Dense Layer
+model.add(layers.Flatten())
+model.add(layers.Dense(64,  activation='relu'))
+
+# Output layer with 10 neurons (for 10 digit classes)
+model.add(layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(train_images, train_labels, epochs=5, batch_size=64, validation_data=(test_images, test_labels))
+
+# Evaluate the model on test data
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f"Test Accuracy:{test_acc * 100:.2f}%")
+
+# Make Predicitons On Test Images
+predictions= model.predict(test_images)
+print(f"Prediction for first test image: {np.argmax(predictions[4])}")
+
+
+plt.imshow(test_images[4].reshape(28,28), cmap='gray')
+plt.title(f"Predicted Label is: {predictions[4].argmax()}")
+plt.show()
